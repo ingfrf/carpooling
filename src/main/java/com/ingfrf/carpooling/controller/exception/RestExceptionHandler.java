de83c0e23a26adcd5820e7de5964ccf79f415f25
ingfrf <ingfrf@gmail.com>
2020-09-26 19:37:47 +0200

    Error handling added

---
+++ src/main/java/com/ingfrf/carpooling/controller/exception/RestExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.ingfrf.carpooling.controller.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = "Malformed JSON Request Body";
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = "Unexpected header(s)";
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
